1b3645d7f728dd45c71baabe59ca0f01d1f306cb
Vitor Balocco <vitorbal@gmail.com>
2016-09-08 00:21:24 +0200

    Add bbs and use bbDispatch for setting the active filter

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import 'todomvc-common/base.css';
 import 'todomvc-app-css/index.css';
 
 import Backbone from 'backbone';
+import { bbDispatch, bbCreateStore } from 'backbone-redux-store';
+
 import AppView from './views/app';
 import Todos from './collections/todos';
-import FilterModel from './models/filter';
+import FilterModel, { filterModelReducer } from './models/filter';
 
 const todos = new Todos();
 const filter = new FilterModel({ filterType: 'all' });
+
+bbCreateStore()(filterModelReducer, filter);
 
 const TodoRouter = Backbone.Router.extend({
     routes: {
@@ -17,7 +21,7 @@ const TodoRouter = Backbone.Router.extend({
     setFilter: function (param) {
         // Set the current filter to be used
         const filterType = param || 'all';
-        filter.set({ filterType })
+        bbDispatch(filter, 'SET_FILTER', filterType);
     }
 });
 new TodoRouter();
--- src/models/filter.js
+++ src/models/filter.js
@@ -2,4 +2,12 @@ import Backbone from 'backbone';
 
 const FilterModel = Backbone.Model.extend({});
 
+export const filterModelReducer = (filterModel = new FilterModel(), action) => {
+    if (action.type === 'SET_FILTER') {
+        filterModel.set({ filterType: action.payload });
+    }
+
+    return filterModel;
+};
+
 export default FilterModel;
